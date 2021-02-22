996b7cd7eaf4f81bdad63ab9396411ff9b322d4c
Klaus <roomsixhu@freenet.de>
2021-02-22 14:14:05 +0100

    right.php

---
+++ deepspace.htm.php
@@ -0,0 +1,57 @@
+<html>
+<head> 
+<style>
+	span.frac {
+	display: inline-block;
+	text-align: center;
+	
+}
+    span.frac > sup {
+	display: block;
+	border-bottom: 1px solid;
+	font: inherit;
+	
+}
+    span.frac > span {
+	display: none;
+}
+span.frac > sub {
+	display: block;
+	font: inherit;
+}
+	span.uml {
+	display: inline-block;
+	text-align: center;
+	vertical-align:-5px;
+}
+span.uml > sup {
+	display: block;
+	font: inherit;
+	position: relative; top: 10px;
+}
+span.uml > span {
+	display: none;
+}
+span.uml > sub {
+	display: block;
+	font: inherit; 
+	vertical-align:5px;
+	
+}</style>
+</head><body>
+Ich <span style='vertical-align:-14px;'><span class='frac'><sup>fl</sup><span> / </span><sub>l</sub></span></span> ausche P <span style='vertical-align:-14px;'><span class='frac'><sup>ul</sup><span> / </span><sub>ol</sub></span></span> <span style='vertical-align:-14px;'><span class='frac'><sup>lis</sup><span> / </span><sub>ice</sub></span></span>.<br />
+The T <span style='vertical-align:-14px;'><span class='frac'><sup>w</sup><span> / </span><sub>o</sub></span></span> ilit of the G <span style='vertical-align:-14px;'><span class='frac'><sup>od</sup><span> / </span><sub>ut</sub></span></span> s !!! / ???<br />
+<span style='vertical-align:-14px;'><span class='frac'><sup>A</sup><span> / </span><sub>B</sub></span></span> <br />
+<span style='vertical-align:-14px;'><span class='frac'><sup> a / aa </sup><span> / </span><sub>bbb</sub></span></span> <br />
+<span style='vertical-align:-14px;'><span class='frac'><sup>ccc</sup><span> / </span><sub>ddd</sub></span></span>
+<br />
+Ich <span style='vertical-align:-14px;'><span class='frac'><sup>fl</sup><span> / </span><sub>l</sub></span></span> ausche P <span style='vertical-align:-14px;'><span class='frac'><sup>ul</sup><span> / </span><sub>ol</sub></span></span> <span style='vertical-align:-14px;'><span class='frac'><sup>lis</sup><span> / </span><sub>ice</sub></span></span>.<br /><br />
+The T <span style='vertical-align:-14px;'><span class='frac'><sup>w</sup><span> / </span><sub>o</sub></span></span> ilit of the G <span style='vertical-align:-14px;'><span class='frac'><sup>od</sup><span> / </span><sub>ut</sub></span></span> s !!! / ???<br /><br />
+<span style='vertical-align:-14px;'><span class='frac'><sup>A</sup><span> / </span><sub>B</sub></span></span> <br /><br />
+</body></html>
+<span style='vertical-align:-14px;'><span class='frac'><sup> <span style='vertical-align:-14px;'><span class='frac'><sup>a</sup><span> / </span><sub>aa</sub></span></span> </sup><span> / </span><sub>bbb</sub></span></span> <br /><br />
+<span style='vertical-align:-14px;'><span class='frac'><sup>ccc</sup><span> / </span><sub>ddd</sub></span></span>
+<hr>
+<div><p><?php include('ator.txt'); ?></p></div>
+<hr>
+</body></html>
