8ccff3dffe2d0beed26bc7dfefabde52e045ad14
Klaus <roomsixhu@freenet.de>
2021-02-22 13:51:23 +0100

    version 2.00

---
+++ style.php
@@ -0,0 +1,32 @@
+<?php
+    header('Content-type: text/css; charset: UTF-8');
+?>
+span.frac { 	display: inline-block; 	text-align: center;  
+}     
+span.frac > sup { 	display: block; 	border-bottom: 1px solid; 	font: inherit;  
+}     
+span.frac > span { 	display: none;
+}
+span.frac > sub { 	display: block; 	font: inherit;
+} 	
+span.uml { 	display: inline-block; 	text-align: center; 	vertical-align:-5px;
+}
+span.uml > sup { 	display: block; 	font: inherit; 	position: relative; top: 10px;
+}
+span.uml > span { 	display: none;
+}
+span.uml > sub { 	display: block; 	font: inherit;  	vertical-align:5px;  
+}
+#box {   width:70%;
+line-height: 1.5;  margin: 0px auto;     //margin-top: 10em; auto;   padding: 15px;   text-align: center;   background-color: #fff5ee;   // border: 1px solid blue;    // border-width:5px;   //  border-spacing: -15px;   //  border-margin:15px;
+}
+body {   margin: 20px;   text-align: center;  /* nur f&uuml;r alte Browser */   background-color: #29220a;//#e6e6fa;
+}
+hr.new1 {   border-top: 3px solid darkgrey;   width:20%;
+}
+.br
+{
+padding: 8px;
+border-right: 5px solid #f00;
+border-left: 5px solid #f00;
+} 
